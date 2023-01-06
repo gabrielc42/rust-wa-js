b6333010a82c91e704b3889bdb2c6e8885bdc1e2
gabrielc42 <clark.gabe97@gmail.com>
2023-01-05 20:15:30 -0600

    web assembly and cargo init

---
+++ snake_game/target/debug/deps/snake_game-ffd5ed5bb0a500b8.d
@@ -0,0 +1,5 @@
+c:\Users\Gabriel\Desktop\reposit\rust-wa-js\snake_game\target\debug\deps\snake_game-ffd5ed5bb0a500b8.rmeta: src\main.rs
+
+c:\Users\Gabriel\Desktop\reposit\rust-wa-js\snake_game\target\debug\deps\snake_game-ffd5ed5bb0a500b8.d: src\main.rs
+
+src\main.rs:
