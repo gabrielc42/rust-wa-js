2c60400e0e3e62ba19dcc326be9c8fcc7e8b3461
gabrielc42 <clark.gabe97@gmail.com>
2023-01-11 15:12:16 -0600

    temp folder clear

--- snake_game/target/debug/deps/snake_game-6752ff8a27b58c87.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Gabriel\Desktop\reposit\rust-wa-js\snake_game\target\debug\deps\snake_game-6752ff8a27b58c87.rmeta: src\main.rs
-
-c:\Users\Gabriel\Desktop\reposit\rust-wa-js\snake_game\target\debug\deps\snake_game-6752ff8a27b58c87.d: src\main.rs
-
-src\main.rs:
--- snake_game/target/debug/deps/snake_game-ffd5ed5bb0a500b8.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Gabriel\Desktop\reposit\rust-wa-js\snake_game\target\debug\deps\snake_game-ffd5ed5bb0a500b8.rmeta: src\main.rs
-
-c:\Users\Gabriel\Desktop\reposit\rust-wa-js\snake_game\target\debug\deps\snake_game-ffd5ed5bb0a500b8.d: src\main.rs
-
-src\main.rs:
