a3b65b9d7c818349a0a0d08d6897cb77144acc76
gabrielc42 <clark.gabe97@gmail.com>
2023-01-29 19:11:10 -0600

    copy webpack plugin, index.html moved up

--- snake_game/view/webpack.config.js
+++ snake_game/view/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
@@ -6,5 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "index.js"
   },
-  mode: "development"
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./index.html", to: "./"}
+      ]
+    })
+  ]
 }
